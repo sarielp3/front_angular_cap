c5e4703d2b7326cbb7717e7c5fe3c0f7e200843a
ManuelCortez <mariomanuelcortezgrano@gmail.com>
2023-01-23 13:39:02 -0600

    Pagina busqueda de vuelos creada

---
+++ src/app/components/vuelos/pages/buscar-vuelos/buscar-vuelos.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-buscar-vuelos',
+  templateUrl: './buscar-vuelos.component.html',
+  styleUrls: ['./buscar-vuelos.component.css']
+})
+export class BuscarVuelosComponent {
+
+}
--- src/app/components/vuelos/vuelos.module.ts
+++ src/app/components/vuelos/vuelos.module.ts
@@ -2,13 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { TablaVuelosComponent } from './components/tabla-vuelos/tabla-vuelos.component';
 import { FiltrosVuelosComponent } from './components/filtros-vuelos/filtros-vuelos.component';
+import { BuscarVuelosComponent } from './pages/buscar-vuelos/buscar-vuelos.component';
 
 
 
 @NgModule({
   declarations: [
     TablaVuelosComponent,
-    FiltrosVuelosComponent
+    FiltrosVuelosComponent,
+    BuscarVuelosComponent
   ],
   imports: [
     CommonModule
